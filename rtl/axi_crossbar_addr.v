620526d581b06dc172aa061c5fd32bf717c9e54a
Alex Forencich <alex@alexforencich.com>
2019-03-12 12:58:56 -0700

    Also match transfers by region

--- rtl/axi_crossbar_addr.v
+++ rtl/axi_crossbar_addr.v
@@ -176,7 +176,7 @@ generate
 
         assign thread_active[n] = thread_count_reg[n] != 0;
         assign thread_match[n] = thread_active[n] && thread_id_reg[n] == s_axi_aid;
-        assign thread_match_dest[n] = thread_match[n] && thread_m_reg[n] == m_select_next;
+        assign thread_match_dest[n] = thread_match[n] && thread_m_reg[n] == m_select_next && (M_REGIONS < 2 || thread_region_reg[n] == m_axi_aregion_next);
         assign thread_cpl_match[n] = thread_active[n] && thread_id_reg[n] == s_cpl_id;
         assign thread_trans_start[n] = (thread_match[n] || (!thread_active[n] && !thread_match && !(thread_trans_start & ({S_INT_THREADS{1'b1}} >> (S_INT_THREADS-n))))) && trans_start;
         assign thread_trans_complete[n] = thread_cpl_match[n] && trans_complete;

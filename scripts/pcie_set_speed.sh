97500d10f6e2ddd67d1bd5b6b4ea0793d75e0aed
Alex Forencich <alex@alexforencich.com>
2019-08-03 23:32:02 -0700

    Improved link speed control script

--- scripts/pcie_set_speed.sh
+++ scripts/pcie_set_speed.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 dev=$1
+speed=$2
 
 if [ -z "$dev" ]; then
     echo "Error: no device specified"
@@ -23,6 +24,24 @@ if [[ $port != pci* ]]; then
     echo "Device $dev is connected to upstream port $port"
 fi
 
+lc=$(setpci -s $dev CAP_EXP+0c.L)
+ls=$(setpci -s $dev CAP_EXP+12.W)
+
+max_speed=$(("0x$lc" & 0xF))
+
+echo "Link capabilities:" $lc
+echo "Max link speed:" $max_speed
+echo "Link status:" $ls
+echo "Current link speed:" $(("0x$ls" & 0xF))
+
+if [ -z "$speed" ]; then
+    speed=$max_speed
+fi
+
+if (($speed > $max_speed)); then
+    speed=$max_speed
+fi
+
 echo "Configuring $dev..."
 
 lc2=$(setpci -s $dev CAP_EXP+30.L)
@@ -30,8 +49,9 @@ lc2=$(setpci -s $dev CAP_EXP+30.L)
 echo "Original link control 2:" $lc2
 echo "Original link target speed:" $(("0x$lc2" & 0xF))
 
-lc2n=$(printf "%08x" $((("0x$lc2" & 0xFFFFFFF0) | 0x2)))
+lc2n=$(printf "%08x" $((("0x$lc2" & 0xFFFFFFF0) | $speed)))
 
+echo "New target link speed:" $speed
 echo "New link control 2:" $lc2n
 
 setpci -s $dev CAP_EXP+30.L=$lc2n
@@ -47,3 +67,10 @@ lcn=$(printf "%08x" $(("0x$lc" | 0x20)))
 echo "New link control:" $lcn
 
 setpci -s $dev CAP_EXP+10.L=$lcn
+
+sleep 0.1
+
+ls=$(setpci -s $dev CAP_EXP+12.W)
+
+echo "Link status:" $ls
+echo "Current link speed:" $(("0x$ls" & 0xF))

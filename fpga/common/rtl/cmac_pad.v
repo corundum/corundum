cbde1abaf97d7d4099dcd9242e1c5d69a24ff397
Alex Forencich <alex@alexforencich.com>
2019-12-23 14:40:51 -0800

    Add CMAC pad module

---
+++ fpga/common/rtl/cmac_pad.v
@@ -0,0 +1,101 @@
+/*
+
+Copyright 2019, The Regents of the University of California.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE REGENTS OF THE UNIVERSITY OF CALIFORNIA ''AS
+IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE REGENTS OF THE UNIVERSITY OF CALIFORNIA OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of The Regents of the University of California.
+
+*/
+
+// Language: Verilog 2001
+
+`timescale 1ns / 1ps
+
+/*
+ * CMAC frame pad module
+ */
+module cmac_pad #
+(
+    // Width of AXI stream interfaces in bits
+    parameter DATA_WIDTH = 512,
+    // tkeep signal width (words per cycle)
+    parameter KEEP_WIDTH = (DATA_WIDTH/8),
+    // tuser signal width
+    parameter USER_WIDTH = 1
+)
+(
+    input  wire                   clk,
+    input  wire                   rst,
+    
+    /*
+     * AXI input
+     */
+    input  wire [DATA_WIDTH-1:0]  s_axis_tdata,
+    input  wire [KEEP_WIDTH-1:0]  s_axis_tkeep,
+    input  wire                   s_axis_tvalid,
+    output wire                   s_axis_tready,
+    input  wire                   s_axis_tlast,
+    input  wire [USER_WIDTH-1:0]  s_axis_tuser,
+
+    /*
+     * AXI output
+     */
+    output wire [DATA_WIDTH-1:0]  m_axis_tdata,
+    output wire [KEEP_WIDTH-1:0]  m_axis_tkeep,
+    output wire                   m_axis_tvalid,
+    input  wire                   m_axis_tready,
+    output wire                   m_axis_tlast,
+    output wire [USER_WIDTH-1:0]  m_axis_tuser
+);
+
+reg frame_reg = 1'b0;
+
+generate
+    genvar k;
+
+    for (k = 0; k < KEEP_WIDTH; k = k + 1) begin
+        assign m_axis_tdata[k*8 +: 8] = s_axis_tkeep[k] ? s_axis_tdata[k*8 +: 8] : 8'd0;
+    end
+endgenerate
+
+assign m_axis_tkeep = frame_reg ? s_axis_tkeep : {KEEP_WIDTH{1'b1}};
+assign m_axis_tvalid = s_axis_tvalid;
+assign s_axis_tready = m_axis_tready;
+assign m_axis_tlast = s_axis_tlast;
+assign m_axis_tuser = s_axis_tuser;
+
+always @(posedge clk) begin
+    if (s_axis_tvalid && s_axis_tready) begin
+        frame_reg <= !s_axis_tlast;
+    end
+
+    if (rst) begin
+        frame_reg <= 1'b0;
+    end
+end
+
+endmodule

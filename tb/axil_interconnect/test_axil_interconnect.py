db58c836f6bc151a4ba113544a8ea5d3ddedc40c
Alex Forencich <alex@alexforencich.com>
2020-12-28 18:52:47 -0800

    Use absolute path to test directory

--- tb/axil_interconnect/test_axil_interconnect.py
+++ tb/axil_interconnect/test_axil_interconnect.py
@@ -208,7 +208,7 @@ if cocotb.SIM_NAME:
 
 # cocotb-test
 
-tests_dir = os.path.dirname(__file__)
+tests_dir = os.path.abspath(os.path.dirname(__file__))
 rtl_dir = os.path.abspath(os.path.join(tests_dir, '..', '..', 'rtl'))
 
 

9883e776c3226b5016fe0625dcde0098546fb712
Alex Forencich <alex@alexforencich.com>
2021-11-03 20:46:40 -0700

    Parameter cleanup

--- example/common/tb/example_core_pcie_us/test_example_core_pcie_us.py
+++ example/common/tb/example_core_pcie_us/test_example_core_pcie_us.py
@@ -469,7 +469,7 @@ def test_example_core_pcie_us(request, axis_pcie_data_width):
     parameters['AXIS_PCIE_RC_USER_WIDTH'] = 75 if parameters['AXIS_PCIE_DATA_WIDTH'] < 512 else 161
     parameters['AXIS_PCIE_CQ_USER_WIDTH'] = 88 if parameters['AXIS_PCIE_DATA_WIDTH'] < 512 else 183
     parameters['AXIS_PCIE_CC_USER_WIDTH'] = 33 if parameters['AXIS_PCIE_DATA_WIDTH'] < 512 else 81
-    parameters['RQ_SEQ_NUM_WIDTH'] = 6
+    parameters['RQ_SEQ_NUM_WIDTH'] = 4 if parameters['AXIS_PCIE_RQ_USER_WIDTH'] == 60 else 6
     parameters['RQ_SEQ_NUM_ENABLE'] = 1
     parameters['PCIE_TAG_COUNT'] = 256
     parameters['READ_OP_TABLE_SIZE'] = parameters['PCIE_TAG_COUNT']

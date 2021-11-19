6e5f9f33f2c27548d4e74f3464884c7f466f4f23
Alex Forencich <alex@alexforencich.com>
2021-11-18 16:25:59 -0800

    Add example design for Alveo U200

---
+++ example/AU200/fpga/ip/pcie4_uscale_plus_0.tcl
@@ -0,0 +1,28 @@
+
+create_ip -name pcie4_uscale_plus -vendor xilinx.com -library ip -module_name pcie4_uscale_plus_0
+
+set_property -dict [list \
+    CONFIG.PL_LINK_CAP_MAX_LINK_SPEED {8.0_GT/s} \
+    CONFIG.PL_LINK_CAP_MAX_LINK_WIDTH {X16} \
+    CONFIG.AXISTEN_IF_EXT_512_RQ_STRADDLE {false} \
+    CONFIG.axisten_if_enable_client_tag {true} \
+    CONFIG.axisten_if_width {512_bit} \
+    CONFIG.axisten_freq {250} \
+    CONFIG.PF0_CLASS_CODE {058000} \
+    CONFIG.PF0_DEVICE_ID {0001} \
+    CONFIG.PF0_MSI_CAP_MULTIMSGCAP {32_vectors} \
+    CONFIG.PF0_SUBSYSTEM_ID {90c8} \
+    CONFIG.PF0_SUBSYSTEM_VENDOR_ID {10ee} \
+    CONFIG.pf0_bar0_64bit {true} \
+    CONFIG.pf0_bar0_prefetchable {true} \
+    CONFIG.pf0_bar0_scale {Megabytes} \
+    CONFIG.pf0_bar0_size {16} \
+    CONFIG.pf0_bar2_64bit {true} \
+    CONFIG.pf0_bar2_prefetchable {true} \
+    CONFIG.pf0_bar2_enabled {true} \
+    CONFIG.pf0_bar2_type {Memory} \
+    CONFIG.pf0_bar2_scale {Megabytes} \
+    CONFIG.pf0_bar2_size {16} \
+    CONFIG.vendor_id {1234} \
+    CONFIG.en_msi_per_vec_masking {true} \
+] [get_ips pcie4_uscale_plus_0]

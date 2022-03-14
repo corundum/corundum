1647377eb9219faa5f142476cb36d2ed078ad279
Alex Forencich <alex@alexforencich.com>
2022-03-13 23:35:14 -0700

    Update sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -69,7 +69,7 @@ except ImportError:
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 graphviz_output_format = 'svg'
 

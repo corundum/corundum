4fc7e0b9d8e19a563cbf9bfc083f8e8d42c36072
Alex Forencich <alex@alexforencich.com>
2022-03-14 00:47:27 -0700

    Use rsvg instead of inkscape for SVG conversion

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ copyright = f'2019-{datetime.datetime.now().year}, {author}'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinxcontrib.inkscapeconverter",
+    "sphinxcontrib.rsvgconverter",
 ]
 
 # Add any paths that contain templates here, relative to this directory.

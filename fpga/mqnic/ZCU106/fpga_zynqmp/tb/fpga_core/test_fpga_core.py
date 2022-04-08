1bb7053a683a7f9a7d28bb52bb5d2ea7b60e2386
Alex Forencich <alex@alexforencich.com>
2022-04-07 21:42:01 -0700

    ZCU106/fpga_zynqmp: Add integration test
    Signed-off-by: Alex Forencich <alex@alexforencich.com>

---
+++ fpga/mqnic/ZCU106/fpga_zynqmp/tb/fpga_core/test_fpga_core.py
@@ -0,0 +1,483 @@
+"""
+
+Copyright 2020-2021, The Regents of the University of California.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE REGENTS OF THE UNIVERSITY OF CALIFORNIA ''AS
+IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE REGENTS OF THE UNIVERSITY OF CALIFORNIA OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of The Regents of the University of California.
+
+"""
+
+import logging
+import os
+import sys
+
+import scapy.utils
+from scapy.layers.l2 import Ether
+from scapy.layers.inet import IP, UDP
+
+import cocotb_test.simulator
+
+import cocotb
+from cocotb.log import SimLog
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge
+
+from cocotbext.axi import AddressSpace
+from cocotbext.axi import AxiLiteMaster, AxiLiteBus
+from cocotbext.axi import AxiSlave, AxiBus
+from cocotbext.eth import XgmiiSource, XgmiiSink
+
+try:
+    import mqnic
+except ImportError:
+    # attempt import from current directory
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__)))
+    try:
+        import mqnic
+    finally:
+        del sys.path[0]
+
+
+class TB(object):
+    def __init__(self, dut):
+        self.dut = dut
+
+        self.log = SimLog("cocotb.tb")
+        self.log.setLevel(logging.DEBUG)
+
+        cocotb.start_soon(Clock(dut.clk_250mhz, 4, units="ns").start())
+
+        # AXI
+        self.address_space = AddressSpace()
+        self.pool = self.address_space.create_pool(0, 0x8000_0000)
+
+        self.axil_master = AxiLiteMaster(AxiLiteBus.from_prefix(dut, "s_axil_ctrl"), dut.clk_250mhz, dut.rst_250mhz)
+        self.address_space.register_region(self.axil_master, 0x10_0000_0000)
+        self.hw_regs = self.address_space.create_window(0x10_0000_0000, self.axil_master.size)
+
+        self.axi_slave = AxiSlave(AxiBus.from_prefix(dut, "m_axi"), dut.clk_250mhz, dut.rst_250mhz, self.address_space)
+
+        self.driver = mqnic.Driver()
+
+        # Ethernet
+        cocotb.start_soon(Clock(dut.sfp0_rx_clk, 6.4, units="ns").start())
+        self.sfp0_source = XgmiiSource(dut.sfp0_rxd, dut.sfp0_rxc, dut.sfp0_rx_clk, dut.sfp0_rx_rst)
+        cocotb.start_soon(Clock(dut.sfp0_tx_clk, 6.4, units="ns").start())
+        self.sfp0_sink = XgmiiSink(dut.sfp0_txd, dut.sfp0_txc, dut.sfp0_tx_clk, dut.sfp0_tx_rst)
+
+        cocotb.start_soon(Clock(dut.sfp1_rx_clk, 6.4, units="ns").start())
+        self.sfp1_source = XgmiiSource(dut.sfp1_rxd, dut.sfp1_rxc, dut.sfp1_rx_clk, dut.sfp1_rx_rst)
+        cocotb.start_soon(Clock(dut.sfp1_tx_clk, 6.4, units="ns").start())
+        self.sfp1_sink = XgmiiSink(dut.sfp1_txd, dut.sfp1_txc, dut.sfp1_tx_clk, dut.sfp1_tx_rst)
+
+        cocotb.start_soon(Clock(dut.sfp_drp_clk, 8, units="ns").start())
+        dut.sfp_drp_rst.setimmediatevalue(0)
+        dut.sfp_drp_do.setimmediatevalue(0)
+        dut.sfp_drp_rdy.setimmediatevalue(0)
+
+        dut.sfp0_rx_error_count.setimmediatevalue(0)
+        dut.sfp1_rx_error_count.setimmediatevalue(0)
+
+        dut.btnu.setimmediatevalue(0)
+        dut.btnl.setimmediatevalue(0)
+        dut.btnd.setimmediatevalue(0)
+        dut.btnr.setimmediatevalue(0)
+        dut.btnc.setimmediatevalue(0)
+        dut.sw.setimmediatevalue(0)
+
+        dut.i2c_scl_i.setimmediatevalue(1)
+        dut.i2c_sda_i.setimmediatevalue(1)
+
+        self.loopback_enable = False
+        cocotb.start_soon(self._run_loopback())
+
+    async def init(self):
+
+        self.dut.rst_250mhz.setimmediatevalue(0)
+        self.dut.sfp0_rx_rst.setimmediatevalue(0)
+        self.dut.sfp0_tx_rst.setimmediatevalue(0)
+        self.dut.sfp1_rx_rst.setimmediatevalue(0)
+        self.dut.sfp1_tx_rst.setimmediatevalue(0)
+
+        await RisingEdge(self.dut.clk_250mhz)
+        await RisingEdge(self.dut.clk_250mhz)
+
+        self.dut.rst_250mhz.value = 1
+        self.dut.sfp0_rx_rst.setimmediatevalue(1)
+        self.dut.sfp0_tx_rst.setimmediatevalue(1)
+        self.dut.sfp1_rx_rst.setimmediatevalue(1)
+        self.dut.sfp1_tx_rst.setimmediatevalue(1)
+
+        await RisingEdge(self.dut.clk_250mhz)
+        await RisingEdge(self.dut.clk_250mhz)
+
+        self.dut.rst_250mhz.value = 0
+        self.dut.sfp0_rx_rst.setimmediatevalue(0)
+        self.dut.sfp0_tx_rst.setimmediatevalue(0)
+        self.dut.sfp1_rx_rst.setimmediatevalue(0)
+        self.dut.sfp1_tx_rst.setimmediatevalue(0)
+
+    async def _run_loopback(self):
+        while True:
+            await RisingEdge(self.dut.clk_250mhz)
+
+            if self.loopback_enable:
+                if not self.sfp0_sink.empty():
+                    await self.sfp0_source.send(await self.sfp0_sink.recv())
+                if not self.sfp1_sink.empty():
+                    await self.sfp1_source.send(await self.sfp1_sink.recv())
+
+
+@cocotb.test()
+async def run_test_nic(dut):
+
+    tb = TB(dut)
+
+    await tb.init()
+
+    tb.log.info("Init driver")
+    await tb.driver.init_axi_dev(tb.pool, tb.hw_regs, irq=dut.irq)
+    await tb.driver.interfaces[0].open()
+    # await tb.driver.interfaces[1].open()
+
+    # enable queues
+    tb.log.info("Enable queues")
+    await tb.driver.interfaces[0].sched_blocks[0].schedulers[0].rb.write_dword(mqnic.MQNIC_RB_SCHED_RR_REG_CTRL, 0x00000001)
+    for k in range(tb.driver.interfaces[0].tx_queue_count):
+        await tb.driver.interfaces[0].sched_blocks[0].schedulers[0].hw_regs.write_dword(4*k, 0x00000003)
+
+    # wait for all writes to complete
+    await tb.driver.hw_regs.read_dword(0)
+    tb.log.info("Init complete")
+
+    tb.log.info("Send and receive single packet")
+
+    data = bytearray([x % 256 for x in range(1024)])
+
+    await tb.driver.interfaces[0].start_xmit(data, 0)
+
+    pkt = await tb.sfp0_sink.recv()
+    tb.log.info("Packet: %s", pkt)
+
+    await tb.sfp0_source.send(pkt)
+
+    pkt = await tb.driver.interfaces[0].recv()
+
+    tb.log.info("Packet: %s", pkt)
+    assert pkt.rx_checksum == ~scapy.utils.checksum(bytes(pkt.data[14:])) & 0xffff
+
+    # await tb.driver.interfaces[1].start_xmit(data, 0)
+
+    # pkt = await tb.sfp1_sink.recv()
+    # tb.log.info("Packet: %s", pkt)
+
+    # await tb.sfp1_source.send(pkt)
+
+    # pkt = await tb.driver.interfaces[1].recv()
+
+    # tb.log.info("Packet: %s", pkt)
+    # assert pkt.rx_checksum == ~scapy.utils.checksum(bytes(pkt.data[14:])) & 0xffff
+
+    tb.log.info("RX and TX checksum tests")
+
+    payload = bytes([x % 256 for x in range(256)])
+    eth = Ether(src='5A:51:52:53:54:55', dst='DA:D1:D2:D3:D4:D5')
+    ip = IP(src='192.168.1.100', dst='192.168.1.101')
+    udp = UDP(sport=1, dport=2)
+    test_pkt = eth / ip / udp / payload
+
+    test_pkt2 = test_pkt.copy()
+    test_pkt2[UDP].chksum = scapy.utils.checksum(bytes(test_pkt2[UDP]))
+
+    await tb.driver.interfaces[0].start_xmit(test_pkt2.build(), 0, 34, 6)
+
+    pkt = await tb.sfp0_sink.recv()
+    tb.log.info("Packet: %s", pkt)
+
+    await tb.sfp0_source.send(pkt)
+
+    pkt = await tb.driver.interfaces[0].recv()
+
+    tb.log.info("Packet: %s", pkt)
+    assert pkt.rx_checksum == ~scapy.utils.checksum(bytes(pkt.data[14:])) & 0xffff
+    assert Ether(pkt.data).build() == test_pkt.build()
+
+    tb.log.info("Multiple small packets")
+
+    count = 64
+
+    pkts = [bytearray([(x+k) % 256 for x in range(60)]) for k in range(count)]
+
+    tb.loopback_enable = True
+
+    for p in pkts:
+        await tb.driver.interfaces[0].start_xmit(p, 0)
+
+    for k in range(count):
+        pkt = await tb.driver.interfaces[0].recv()
+
+        tb.log.info("Packet: %s", pkt)
+        assert pkt.data == pkts[k]
+        assert pkt.rx_checksum == ~scapy.utils.checksum(bytes(pkt.data[14:])) & 0xffff
+
+    tb.loopback_enable = False
+
+    tb.log.info("Multiple large packets")
+
+    count = 64
+
+    pkts = [bytearray([(x+k) % 256 for x in range(1514)]) for k in range(count)]
+
+    tb.loopback_enable = True
+
+    for p in pkts:
+        await tb.driver.interfaces[0].start_xmit(p, 0)
+
+    for k in range(count):
+        pkt = await tb.driver.interfaces[0].recv()
+
+        tb.log.info("Packet: %s", pkt)
+        assert pkt.data == pkts[k]
+        assert pkt.rx_checksum == ~scapy.utils.checksum(bytes(pkt.data[14:])) & 0xffff
+
+    tb.loopback_enable = False
+
+    await RisingEdge(dut.clk_250mhz)
+    await RisingEdge(dut.clk_250mhz)
+
+
+# cocotb-test
+
+tests_dir = os.path.dirname(__file__)
+rtl_dir = os.path.abspath(os.path.join(tests_dir, '..', '..', 'rtl'))
+lib_dir = os.path.abspath(os.path.join(rtl_dir, '..', 'lib'))
+app_dir = os.path.abspath(os.path.join(rtl_dir, '..', 'app'))
+axi_rtl_dir = os.path.abspath(os.path.join(lib_dir, 'axi', 'rtl'))
+axis_rtl_dir = os.path.abspath(os.path.join(lib_dir, 'axis', 'rtl'))
+eth_rtl_dir = os.path.abspath(os.path.join(lib_dir, 'eth', 'rtl'))
+pcie_rtl_dir = os.path.abspath(os.path.join(lib_dir, 'pcie', 'rtl'))
+
+
+def test_fpga_core(request):
+    dut = "fpga_core"
+    module = os.path.splitext(os.path.basename(__file__))[0]
+    toplevel = dut
+
+    verilog_sources = [
+        os.path.join(rtl_dir, f"{dut}.v"),
+        os.path.join(rtl_dir, "common", "mqnic_core_axi.v"),
+        os.path.join(rtl_dir, "common", "mqnic_core.v"),
+        os.path.join(rtl_dir, "common", "mqnic_interface.v"),
+        os.path.join(rtl_dir, "common", "mqnic_interface_tx.v"),
+        os.path.join(rtl_dir, "common", "mqnic_interface_rx.v"),
+        os.path.join(rtl_dir, "common", "mqnic_egress.v"),
+        os.path.join(rtl_dir, "common", "mqnic_ingress.v"),
+        os.path.join(rtl_dir, "common", "mqnic_l2_egress.v"),
+        os.path.join(rtl_dir, "common", "mqnic_l2_ingress.v"),
+        os.path.join(rtl_dir, "common", "mqnic_ptp.v"),
+        os.path.join(rtl_dir, "common", "mqnic_ptp_clock.v"),
+        os.path.join(rtl_dir, "common", "mqnic_ptp_perout.v"),
+        os.path.join(rtl_dir, "common", "cpl_write.v"),
+        os.path.join(rtl_dir, "common", "cpl_op_mux.v"),
+        os.path.join(rtl_dir, "common", "desc_fetch.v"),
+        os.path.join(rtl_dir, "common", "desc_op_mux.v"),
+        os.path.join(rtl_dir, "common", "event_mux.v"),
+        os.path.join(rtl_dir, "common", "queue_manager.v"),
+        os.path.join(rtl_dir, "common", "cpl_queue_manager.v"),
+        os.path.join(rtl_dir, "common", "tx_fifo.v"),
+        os.path.join(rtl_dir, "common", "rx_fifo.v"),
+        os.path.join(rtl_dir, "common", "tx_req_mux.v"),
+        os.path.join(rtl_dir, "common", "tx_engine.v"),
+        os.path.join(rtl_dir, "common", "rx_engine.v"),
+        os.path.join(rtl_dir, "common", "tx_checksum.v"),
+        os.path.join(rtl_dir, "common", "rx_hash.v"),
+        os.path.join(rtl_dir, "common", "rx_checksum.v"),
+        os.path.join(rtl_dir, "common", "rb_drp.v"),
+        os.path.join(rtl_dir, "common", "stats_counter.v"),
+        os.path.join(rtl_dir, "common", "stats_collect.v"),
+        os.path.join(rtl_dir, "common", "stats_dma_latency.v"),
+        os.path.join(rtl_dir, "common", "mqnic_tx_scheduler_block_rr.v"),
+        os.path.join(rtl_dir, "common", "tx_scheduler_rr.v"),
+        os.path.join(rtl_dir, "common", "tdma_scheduler.v"),
+        os.path.join(rtl_dir, "common", "tdma_ber.v"),
+        os.path.join(rtl_dir, "common", "tdma_ber_ch.v"),
+        os.path.join(eth_rtl_dir, "eth_mac_10g.v"),
+        os.path.join(eth_rtl_dir, "axis_xgmii_rx_64.v"),
+        os.path.join(eth_rtl_dir, "axis_xgmii_tx_64.v"),
+        os.path.join(eth_rtl_dir, "lfsr.v"),
+        os.path.join(eth_rtl_dir, "ptp_clock.v"),
+        os.path.join(eth_rtl_dir, "ptp_clock_cdc.v"),
+        os.path.join(eth_rtl_dir, "ptp_perout.v"),
+        os.path.join(axi_rtl_dir, "axil_interconnect.v"),
+        os.path.join(axi_rtl_dir, "axil_crossbar.v"),
+        os.path.join(axi_rtl_dir, "axil_crossbar_addr.v"),
+        os.path.join(axi_rtl_dir, "axil_crossbar_rd.v"),
+        os.path.join(axi_rtl_dir, "axil_crossbar_wr.v"),
+        os.path.join(axi_rtl_dir, "axil_reg_if.v"),
+        os.path.join(axi_rtl_dir, "axil_reg_if_rd.v"),
+        os.path.join(axi_rtl_dir, "axil_reg_if_wr.v"),
+        os.path.join(axi_rtl_dir, "axil_register_rd.v"),
+        os.path.join(axi_rtl_dir, "axil_register_wr.v"),
+        os.path.join(axi_rtl_dir, "arbiter.v"),
+        os.path.join(axi_rtl_dir, "priority_encoder.v"),
+        os.path.join(axis_rtl_dir, "axis_adapter.v"),
+        os.path.join(axis_rtl_dir, "axis_arb_mux.v"),
+        os.path.join(axis_rtl_dir, "axis_async_fifo.v"),
+        os.path.join(axis_rtl_dir, "axis_async_fifo_adapter.v"),
+        os.path.join(axis_rtl_dir, "axis_demux.v"),
+        os.path.join(axis_rtl_dir, "axis_fifo.v"),
+        os.path.join(axis_rtl_dir, "axis_fifo_adapter.v"),
+        os.path.join(axis_rtl_dir, "axis_pipeline_fifo.v"),
+        os.path.join(axis_rtl_dir, "axis_register.v"),
+        os.path.join(pcie_rtl_dir, "dma_if_axi.v"),
+        os.path.join(pcie_rtl_dir, "dma_if_axi_rd.v"),
+        os.path.join(pcie_rtl_dir, "dma_if_axi_wr.v"),
+        os.path.join(pcie_rtl_dir, "dma_if_mux.v"),
+        os.path.join(pcie_rtl_dir, "dma_if_mux_rd.v"),
+        os.path.join(pcie_rtl_dir, "dma_if_mux_wr.v"),
+        os.path.join(pcie_rtl_dir, "dma_if_desc_mux.v"),
+        os.path.join(pcie_rtl_dir, "dma_ram_demux_rd.v"),
+        os.path.join(pcie_rtl_dir, "dma_ram_demux_wr.v"),
+        os.path.join(pcie_rtl_dir, "dma_psdpram.v"),
+        os.path.join(pcie_rtl_dir, "dma_client_axis_sink.v"),
+        os.path.join(pcie_rtl_dir, "dma_client_axis_source.v"),
+        os.path.join(pcie_rtl_dir, "pulse_merge.v"),
+    ]
+
+    parameters = {}
+
+    # Structural configuration
+    parameters['IF_COUNT'] = 2
+    parameters['PORTS_PER_IF'] = 1
+    parameters['SCHED_PER_IF'] = parameters['PORTS_PER_IF']
+
+    # PTP configuration
+    parameters['PTP_CLOCK_PIPELINE'] = 0
+    parameters['PTP_USE_SAMPLE_CLOCK'] = 0
+    parameters['PTP_PORT_CDC_PIPELINE'] = 0
+    parameters['PTP_PEROUT_ENABLE'] = 1
+    parameters['PTP_PEROUT_COUNT'] = 1
+
+    # Queue manager configuration (interface)
+    parameters['EVENT_QUEUE_OP_TABLE_SIZE'] = 32
+    parameters['TX_QUEUE_OP_TABLE_SIZE'] = 32
+    parameters['RX_QUEUE_OP_TABLE_SIZE'] = 32
+    parameters['TX_CPL_QUEUE_OP_TABLE_SIZE'] = parameters['TX_QUEUE_OP_TABLE_SIZE']
+    parameters['RX_CPL_QUEUE_OP_TABLE_SIZE'] = parameters['RX_QUEUE_OP_TABLE_SIZE']
+    parameters['EVENT_QUEUE_INDEX_WIDTH'] = 5
+    parameters['TX_QUEUE_INDEX_WIDTH'] = 13
+    parameters['RX_QUEUE_INDEX_WIDTH'] = 8
+    parameters['TX_CPL_QUEUE_INDEX_WIDTH'] = parameters['TX_QUEUE_INDEX_WIDTH']
+    parameters['RX_CPL_QUEUE_INDEX_WIDTH'] = parameters['RX_QUEUE_INDEX_WIDTH']
+    parameters['EVENT_QUEUE_PIPELINE'] = 3
+    parameters['TX_QUEUE_PIPELINE'] = 3 + max(parameters['TX_QUEUE_INDEX_WIDTH']-12, 0)
+    parameters['RX_QUEUE_PIPELINE'] = 3 + max(parameters['RX_QUEUE_INDEX_WIDTH']-12, 0)
+    parameters['TX_CPL_QUEUE_PIPELINE'] = parameters['TX_QUEUE_PIPELINE']
+    parameters['RX_CPL_QUEUE_PIPELINE'] = parameters['RX_QUEUE_PIPELINE']
+
+    # TX and RX engine configuration (port)
+    parameters['TX_DESC_TABLE_SIZE'] = 32
+    parameters['RX_DESC_TABLE_SIZE'] = 32
+
+    # Scheduler configuration (port)
+    parameters['TX_SCHEDULER_OP_TABLE_SIZE'] = parameters['TX_DESC_TABLE_SIZE']
+    parameters['TX_SCHEDULER_PIPELINE'] = parameters['TX_QUEUE_PIPELINE']
+    parameters['TDMA_INDEX_WIDTH'] = 6
+
+    # Timestamping configuration (port)
+    parameters['PTP_TS_ENABLE'] = 1
+    parameters['TX_PTP_TS_FIFO_DEPTH'] = 32
+    parameters['RX_PTP_TS_FIFO_DEPTH'] = 32
+
+    # Interface configuration (port)
+    parameters['TX_CHECKSUM_ENABLE'] = 1
+    parameters['RX_RSS_ENABLE'] = 1
+    parameters['RX_HASH_ENABLE'] = 1
+    parameters['RX_CHECKSUM_ENABLE'] = 1
+    parameters['TX_FIFO_DEPTH'] = 32768
+    parameters['RX_FIFO_DEPTH'] = 32768
+    parameters['MAX_TX_SIZE'] = 9214
+    parameters['MAX_RX_SIZE'] = 9214
+    parameters['TX_RAM_SIZE'] = 32768
+    parameters['RX_RAM_SIZE'] = 32768
+
+    # Application block configuration
+    parameters['APP_ENABLE'] = 0
+    parameters['APP_CTRL_ENABLE'] = 1
+    parameters['APP_DMA_ENABLE'] = 1
+    parameters['APP_AXIS_DIRECT_ENABLE'] = 1
+    parameters['APP_AXIS_SYNC_ENABLE'] = 1
+    parameters['APP_AXIS_IF_ENABLE'] = 1
+    parameters['APP_STAT_ENABLE'] = 1
+
+    # AXI DMA interface configuration
+    parameters['AXI_DATA_WIDTH'] = 128
+    parameters['AXI_ADDR_WIDTH'] = 40
+    parameters['AXI_ID_WIDTH'] = 4
+
+    # DMA interface configuration
+    parameters['DMA_LEN_WIDTH'] = 16
+    parameters['DMA_TAG_WIDTH'] = 16
+    parameters['RAM_ADDR_WIDTH'] = (max(parameters['TX_RAM_SIZE'], parameters['RX_RAM_SIZE'])-1).bit_length()
+    parameters['RAM_PIPELINE'] = 2
+    parameters['AXI_DMA_MAX_BURST_LEN'] = 16
+
+    # AXI lite interface configuration (control)
+    parameters['AXIL_CTRL_DATA_WIDTH'] = 32
+    parameters['AXIL_CTRL_ADDR_WIDTH'] = 24
+
+    # AXI lite interface configuration (application control)
+    parameters['AXIL_APP_CTRL_DATA_WIDTH'] = parameters['AXIL_CTRL_DATA_WIDTH']
+    parameters['AXIL_APP_CTRL_ADDR_WIDTH'] = 24
+
+    # Ethernet interface configuration
+    parameters['AXIS_ETH_TX_PIPELINE'] = 0
+    parameters['AXIS_ETH_TX_FIFO_PIPELINE'] = 2
+    parameters['AXIS_ETH_TX_TS_PIPELINE'] = 0
+    parameters['AXIS_ETH_RX_PIPELINE'] = 0
+    parameters['AXIS_ETH_RX_FIFO_PIPELINE'] = 2
+
+    # Statistics counter subsystem
+    parameters['STAT_ENABLE'] = 1
+    parameters['STAT_DMA_ENABLE'] = 1
+    parameters['STAT_INC_WIDTH'] = 24
+    parameters['STAT_ID_WIDTH'] = 12
+
+    extra_env = {f'PARAM_{k}': str(v) for k, v in parameters.items()}
+
+    sim_build = os.path.join(tests_dir, "sim_build",
+        request.node.name.replace('[', '-').replace(']', ''))
+
+    cocotb_test.simulator.run(
+        python_search=[tests_dir],
+        verilog_sources=verilog_sources,
+        toplevel=toplevel,
+        module=module,
+        parameters=parameters,
+        sim_build=sim_build,
+        extra_env=extra_env,
+    )

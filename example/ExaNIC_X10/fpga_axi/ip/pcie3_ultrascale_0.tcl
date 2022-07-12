e2588cd995b594e9fad63873db6d52c3cd0c45d7
Alex Forencich <alex@alexforencich.com>
2022-07-12 16:23:54 -0700

    Clean up TCL scripts
    Signed-off-by: Alex Forencich <alex@alexforencich.com>

--- example/ExaNIC_X10/fpga_axi/ip/pcie3_ultrascale_0.tcl
+++ example/ExaNIC_X10/fpga_axi/ip/pcie3_ultrascale_0.tcl
@@ -5,12 +5,13 @@ set_property -dict [list \
     CONFIG.PL_LINK_CAP_MAX_LINK_SPEED {8.0_GT/s} \
     CONFIG.PL_LINK_CAP_MAX_LINK_WIDTH {X8} \
     CONFIG.AXISTEN_IF_RC_STRADDLE {false} \
+    CONFIG.axisten_if_enable_client_tag {true} \
     CONFIG.axisten_if_width {256_bit} \
     CONFIG.extended_tag_field {true} \
+    CONFIG.pf0_dev_cap_max_payload {1024_bytes} \
     CONFIG.axisten_freq {250} \
     CONFIG.PF0_CLASS_CODE {058000} \
     CONFIG.PF0_DEVICE_ID {0001} \
-    CONFIG.PF0_MSI_CAP_MULTIMSGCAP {32_vectors} \
     CONFIG.PF0_SUBSYSTEM_ID {0003} \
     CONFIG.PF0_SUBSYSTEM_VENDOR_ID {1ce4} \
     CONFIG.pf0_bar0_64bit {true} \
@@ -23,9 +24,8 @@ set_property -dict [list \
     CONFIG.pf0_bar2_type {Memory} \
     CONFIG.pf0_bar2_scale {Megabytes} \
     CONFIG.pf0_bar2_size {16} \
-    CONFIG.PF0_INTERRUPT_PIN {NONE} \
-    CONFIG.PF0_MSIX_CAP_TABLE_BIR {BAR_0} \
-    CONFIG.PF0_MSIX_CAP_PBA_BIR {BAR_0} \
+    CONFIG.pf0_msi_enabled {true} \
+    CONFIG.PF0_MSI_CAP_MULTIMSGCAP {32_vectors} \
     CONFIG.vendor_id {1234} \
     CONFIG.en_msi_per_vec_masking {true} \
 ] [get_ips pcie3_ultrascale_0]
--- example/fb2CG/fpga_axi/ip/pcie4_uscale_plus_0.tcl
+++ example/fb2CG/fpga_axi/ip/pcie4_uscale_plus_0.tcl
@@ -7,10 +7,11 @@ set_property -dict [list \
     CONFIG.AXISTEN_IF_EXT_512_RQ_STRADDLE {false} \
     CONFIG.axisten_if_enable_client_tag {true} \
     CONFIG.axisten_if_width {512_bit} \
+    CONFIG.extended_tag_field {true} \
+    CONFIG.pf0_dev_cap_max_payload {1024_bytes} \
     CONFIG.axisten_freq {250} \
     CONFIG.PF0_CLASS_CODE {058000} \
     CONFIG.PF0_DEVICE_ID {0001} \
-    CONFIG.PF0_MSI_CAP_MULTIMSGCAP {32_vectors} \
     CONFIG.PF0_SUBSYSTEM_ID {a00e} \
     CONFIG.PF0_SUBSYSTEM_VENDOR_ID {1c2c} \
     CONFIG.pf0_bar0_64bit {true} \
@@ -23,6 +24,8 @@ set_property -dict [list \
     CONFIG.pf0_bar2_type {Memory} \
     CONFIG.pf0_bar2_scale {Megabytes} \
     CONFIG.pf0_bar2_size {16} \
+    CONFIG.pf0_msi_enabled {true} \
+    CONFIG.PF0_MSI_CAP_MULTIMSGCAP {32_vectors} \
     CONFIG.vendor_id {1234} \
     CONFIG.en_msi_per_vec_masking {true} \
 ] [get_ips pcie4_uscale_plus_0]

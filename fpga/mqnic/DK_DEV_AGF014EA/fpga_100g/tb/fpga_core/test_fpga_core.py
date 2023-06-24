344fcd45fcf7d52e4d7926fcc012f9e805047472
Alex Forencich <alex@alexforencich.com>
2023-06-24 01:25:25 -0700

    fpga/mqnic: Testbench parameter clean-up
    Signed-off-by: Alex Forencich <alex@alexforencich.com>

--- fpga/mqnic/DK_DEV_AGF014EA/fpga_100g/tb/fpga_core/test_fpga_core.py
+++ fpga/mqnic/DK_DEV_AGF014EA/fpga_100g/tb/fpga_core/test_fpga_core.py
@@ -756,8 +756,6 @@ def test_fpga_core(request):
     parameters['PTP_CLOCK_PIPELINE'] = 0
     parameters['PTP_CLOCK_CDC_PIPELINE'] = 0
     parameters['PTP_USE_SAMPLE_CLOCK'] = 1
-    parameters['PTP_SEPARATE_TX_CLOCK'] = 0
-    parameters['PTP_SEPARATE_RX_CLOCK'] = 0
     parameters['PTP_PORT_CDC_PIPELINE'] = 0
     parameters['PTP_PEROUT_ENABLE'] = 1
     parameters['PTP_PEROUT_COUNT'] = 1

a839ecf4cc10ae40873f04af2433b666eebe0f42
Alex Forencich <alex@alexforencich.com>
2023-11-28 00:20:31 -0800

    fpga/mqnic/VCU118: Fix VCU118 refclk
    Signed-off-by: Alex Forencich <alex@alexforencich.com>

--- fpga/mqnic/VCU118/fpga_25g/rtl/fpga.v
+++ fpga/mqnic/VCU118/fpga_25g/rtl/fpga.v
@@ -1218,14 +1218,14 @@ qsfp2_phy_quad_inst (
      * Common
      */
     .xcvr_gtpowergood_out(),
-    .xcvr_gtrefclk00_in(qsfp1_mgt_refclk_1),
+    .xcvr_gtrefclk00_in(qsfp1_mgt_refclk_0),
     .xcvr_qpll0pd_in(1'b0),
     .xcvr_qpll0reset_in(1'b0),
     .xcvr_qpll0pcierate_in(3'd0),
     .xcvr_qpll0lock_out(),
     .xcvr_qpll0clk_out(),
     .xcvr_qpll0refclk_out(),
-    .xcvr_gtrefclk01_in(qsfp1_mgt_refclk_1),
+    .xcvr_gtrefclk01_in(qsfp1_mgt_refclk_0),
     .xcvr_qpll1pd_in(1'b0),
     .xcvr_qpll1reset_in(1'b0),
     .xcvr_qpll1pcierate_in(3'd0),
